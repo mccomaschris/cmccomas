c7757b48e7ff5f45f760eb5adec93aeff84dc4ed
Christopher McComas <mccomas.chris@gmail.com>
2023-06-23 09:49:33 -0400

    fix postcss issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,1 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: cloudflare()
-});
